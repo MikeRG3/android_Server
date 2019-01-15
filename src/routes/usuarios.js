d4e125daef3ce4c42cc8d770d9c927f3b17157b5
MikeRG3 <37549134+MikeRG3@users.noreply.github.com>
2019-01-15 21:01:02 +0100

    Update usuarios.js

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -5,6 +5,7 @@ const Usuario = require('../models/usuarios'); // Schema de Usuario
 
 
 // GET USERS
+//Obtiene todos los usuarios
 
 router.get('/usuarios', function(req, res) {
 
@@ -19,11 +20,48 @@ router.get('/usuarios', function(req, res) {
         })
 
 });
-
+//Obtiene los datos del usuario por ID
 router.get('/usuarios/:id', function(req, res) {
     let id = req.params.id;
-    res.json(id);
+    Usuario.findById(id, function(err, usuarioDB) {
+        if (err) throw err;
+        res.json({
+            ok: true,
+            usuarioDB
+        })
+    })
 });
+
+//LOGIN
+//Comrpueba usuario y contraseña
+router.post('/login', function(req, res) {
+    let body = req.body;
+    let usuario = new Usuario({
+        nick: body.nick,
+        password: body.password
+    });
+
+    Usuario.find({ "nick": usuario.nick, "password": usuario.password }, (err, usuarioDB) => {
+        if (err) throw err;
+
+        let message;
+        let ok;
+        if (usuarioDB.length == 0) {
+            ok = false;
+            message = "Access Denied";
+        } else {
+            ok = true;
+            message = "Access Granted";
+        }
+        res.json({
+            ok,
+            message,
+            admin: usuarioDB.admin
+        })
+    })
+
+})
+
 
 // INSERT USERS
 
@@ -52,5 +90,44 @@ router.post('/usuarios', function(req, res) {
     })
 })
 
+//UPDATE USUARIOS
 
+router.put('/usuarios/:id', function(req, res) {
+    let id = req.params.id;
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nick: body.nick,
+        password: body.password,
+        email: body.email,
+        admin: body.admin
+    });
+    Usuario.findByOneAndUpdate(id, { password: usuario.password }, (err, usuarioDB) => {
+        if (err) throw err;
+
+        res.json({
+            ok: true,
+            usuarioDB
+        })
+    })
+
+});
+
+
+//DELETE 
+router.delete('/usuarios/:id', function(req, res) {
+    let id = req.params.id;
+    let body = req.body;
+
+
+    Usuario.findOneAndDelete(id, (err, usuarioDB) => {
+        if (err) throw err;
+
+        res.json({
+            ok: true,
+            message: "User delete"
+        })
+    })
+
+});
 module.exports = router;
